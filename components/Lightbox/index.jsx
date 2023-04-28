35d389421f0cd89d82e2dfd60dde87958c48bbcc
mikeyv <michaelvik520@gmail.com>
2023-04-28 13:20:28 +0100

    more ui fixes

--- components/Lightbox/index.jsx
+++ components/Lightbox/index.jsx
@@ -2,20 +2,40 @@ import Styles from './styles.module.css'
 import Image from 'next/image'
 import { useRef, useState, useEffect } from 'react'
 import { FaArrowDown, FaAngleRight, FaAngleLeft, FaEdit } from 'react-icons/fa'
+import { HiZoomIn, HiZoomOut } from 'react-icons/hi'
 import { IconContext } from 'react-icons'
 import { CloseBtn } from './close'
 
 export const Lightbox = ({ images, close }) => {
 	const [imageIndex, setImageIndex] = useState(0)
+	const [currentZoomLevel, setCurrentZoomLevel] = useState(0)
 	const sliderWrapperRef = useRef()
 	const largeImageViewWrapperRef = useRef()
 	const activePreviewImgRef = useRef()
+	const largeImgRef = useRef()
+
+
 	const maxIndex = images.length - 1
+	const maxZoomInLevel = 5
+	const maxZoomOutLevel = -5
 
-
+	//if currently selected image preview is not visible, scroll it into view
 	useEffect(() => {
 		activePreviewImgRef.current.scrollIntoView({ behavior: "smooth" })
 	}, [imageIndex])
+
+
+	useEffect(() => {
+		let width = largeImgRef.current.clientWidth
+		console.log(width)
+
+		const multiplier = 0.2 + currentZoomLevel
+
+		width = width * multiplier
+
+		console.log(width)
+	}, [currentZoomLevel])
+
 
 	function scrollForward() {
 		console.log("scroll forward")
@@ -46,12 +66,40 @@ export const Lightbox = ({ images, close }) => {
 		})
 	}
 
+	function zoomIn() {
+		setCurrentZoomLevel(
+			prev => {
+				if (prev === maxZoomInLevel) return prev
+				return prev + 1
+			}
+		)
+	}
+
+	function zoomOut() {
+		setCurrentZoomLevel(
+			prev => {
+				if (prev === maxZoomOutLevel) return prev
+				return prev - 1
+			}
+		)
+	}
+
 	return (
 		<div className={Styles.Wrapper}>
 			<div className={Styles.ContentWrapper}>
 
 				<div className={Styles.ToolsMenu}>
 					<div className={Styles.Options}>
+						<button type='button' onClick={() => zoomIn()} className={Styles.CopyBtn}>
+							<IconContext.Provider value={{ className: Styles.OptionBtnIcon }}>
+								<HiZoomIn />
+							</IconContext.Provider>
+						</button>
+						<button type='button' onClick={() => zoomOut()} className={Styles.CopyBtn}>
+							<IconContext.Provider value={{ className: Styles.OptionBtnIcon }}>
+								<HiZoomOut />
+							</IconContext.Provider>
+						</button>
 						<button type='button' onClick={() => copyImageUrl(images[imageIndex].image)} className={Styles.CopyBtn}>
 							<IconContext.Provider value={{ className: Styles.OptionBtnIcon }}>
 								<FaEdit />
@@ -71,7 +119,7 @@ export const Lightbox = ({ images, close }) => {
 
 				<div className={Styles.LargeImageViewWrapper} ref={largeImageViewWrapperRef}>
 					<div className={Styles.LargeImageWrapper} >
-						<Image className={Styles.LargeImage} alt={images[imageIndex].text} src={images[imageIndex].image} fill />
+						<Image className={Styles.LargeImage} alt={images[imageIndex].text} src={images[imageIndex].image} fill ref={largeImgRef} />
 
 						<button onClick={prevImage} className={Styles.Btn}>
 							<IconContext.Provider value={{ className: `${Styles.LargeBtnIcon} ${Styles.Left}` }}>
